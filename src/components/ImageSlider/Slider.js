82724d548e7b7ec13d74d5474c15387b7680e87b
Gokul-MDA <gokul.mda@gmail.com>
2021-06-04 01:36:21 +0530

    add slider

---
+++ src/components/ImageSlider/Slider.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import "./Slider.css";
+import { SliderData } from "./SliderData.js";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
+
+const Slider = ({ slides }) => {
+  const [current, setcurrent] = useState(0);
+  const length = slides.length;
+
+  const prevSlide = () => {
+    setcurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  const nextSlide = () => {
+    setcurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  return (
+    <div className="slider">
+      <ArrowBackIosIcon onClick={prevSlide} className="backArrow" />
+      <ArrowBackIosIcon onClick={prevSlide} className="backArrow1" />
+      <ArrowForwardIosIcon onClick={nextSlide} className="forwardArrow" />
+      <ArrowForwardIosIcon onClick={nextSlide} className="forwardArrow1" />
+      {SliderData.map((slide, index) => {
+        return (
+          <div
+            className={index === current ? "slide active" : "slide"}
+            key={index}
+          >
+            {index === current && (
+              <img src={slide.image} alt="" className="image" />
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Slider;
---
+++ src/components/ImageSlider/SliderData.js
@@ -0,0 +1,23 @@
+import image1 from "../../images/slider1.png";
+import image2 from "../../images/slider2.jpg";
+import image3 from "../../images/slider3.jpeg";
+import image4 from "../../images/slider4.jpg";
+import image5 from "../../images/slider5.jpg";
+
+export const SliderData = [
+  {
+    image: image1,
+  },
+  {
+    image: image2,
+  },
+  {
+    image: image3,
+  },
+  {
+    image: image4,
+  },
+  {
+    image: image5,
+  },
+];
